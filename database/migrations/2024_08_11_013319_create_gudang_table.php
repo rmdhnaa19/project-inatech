944c37d5ba94cb409fa2f94cca711698c6f64f4b
Rasyid Razeka Alamsyah <91864716+rasyidrazeka@users.noreply.github.com>
2024-08-11 09:17:05 +0700

    baru

---
+++ database/migrations/2024_08_11_013319_create_gudang_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gudang', function (Blueprint $table) {
+            $table->id('id_gudang');
+            $table->string('gambar', 100);
+            $table->string('nama', 50)->unique();
+            $table->bigInteger('panjang');
+            $table->bigInteger('lebar');
+            $table->bigInteger('luas');
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gudang');
+    }
+};
---
+++ database/migrations/2024_08_11_014928_create_obat_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('obat', function (Blueprint $table) {
+            $table->id('id_obat');
+            $table->string('gambar',100);
+            $table->string('nama', 50)->unique();
+            $table->bigInteger('harga_satuan');
+            $table->string('satuan', 50);
+            $table->text('deskripsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('obat');
+    }
+};
