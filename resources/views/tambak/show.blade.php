7e9bb38333ce925466b8bb9e085789aa793bc79f
rmdhnaa19 <rmdhnaa@gmail.com>
2024-09-13 13:36:58 +0700

    tambak

--- resources/views/tambak/show.blade.php
+++ resources/views/tambak/show.blade.php
@@ -2,22 +2,16 @@
     <h5>{{ $tambak->nama_tambak }}</h5>
 </div> --}}
 
-
-
-<div class="container">
-    <div class="text-center">
-        <!-- Display foto -->
-        @if($tambak->foto)
-            <img src="{{ asset('storage/' . $tambak->foto) }}" alt="Foto Tambak" class="img-thumbnail" style="max-width: 100%; height: auto;">
-        @else
-            <p>No photo available</p>
-        @endif
-    </div>
-    <div class="mt-3">
-        <h5>Nama Tambak: {{ $tambak->nama_tambak }}</h5>
-        <p><strong>ID Gudang:</strong> {{ $tambak->id_gudang }}</p>
-        <p><strong>Luas Lahan:</strong> {{ $tambak->luas_lahan }} m²</p>
-        <p><strong>Luas Tambak:</strong> {{ $tambak->luas_tambak }} m²</p>
-        <p><strong>Lokasi Tambak:</strong> {{ $tambak->lokasi_tambak }}</p>
-    </div>
+<div>
+    <h5>Detail Tambak</h5>
+    <p><strong>Nama Tambak:</strong> {{ $tambak->nama_tambak }}</p>
+    <p><strong>Nama Gudang:</strong> {{ $tambak->gudang->nama }}</p>
+    <p><strong>Luas Lahan:</strong> {{ $tambak->luas_lahan }}</p>
+    <p><strong>Luas Tambak:</strong> {{ $tambak->luas_tambak }}</p>
+    <p><strong>Lokasi Tambak:</strong> {{ $tambak->lokasi_tambak }}</p>
+    @if($tambak->foto)
+        <p><strong>Foto:</strong></p>
+        <img src="{{ asset('storage/' . $tambak->foto) }}" alt="Foto Tambak" class="img-fluid">
+    @endif
 </div>
+
