820efee597534a1b56398a637a46bb16557a5999
Rasyid Razeka Alamsyah <91864716+rasyidrazeka@users.noreply.github.com>
2024-11-06 15:03:40 +0700

    css transaksi pakan alat obat

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -23,10 +23,13 @@
     <link rel="stylesheet" href="{{ asset('css/kelolaPJGudang.css') }}">
     <link rel="stylesheet" href="{{ asset('css/kelolaPakan.css') }}">
     <link rel="stylesheet" href="{{ asset('css/kelolaPakanGudang.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/kelolaTransaksiPakan.css') }}">
     <link rel="stylesheet" href="{{ asset('css/kelolaAlat.css') }}">
     <link rel="stylesheet" href="{{ asset('css/kelolaAlatGudang.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/kelolaTransaksiAlat.css') }}">
     <link rel="stylesheet" href="{{ asset('css/kelolaObat.css') }}">
     <link rel="stylesheet" href="{{ asset('css/kelolaObatGudang.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/kelolaTransaksiObat.css') }}">
     <link rel="stylesheet" href="{{ asset('css/manajemenKolam.css') }}">
     <link rel="stylesheet" href="{{ asset('css/manajemenTambak.css') }}">
     <link rel="stylesheet" href="{{ asset('css/faseKolam.css') }}">
